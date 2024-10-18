4558bf3311d7111fdd8d94ca1d71edca3eee27bd
Stefan Etarsky <stefan.etarsky@gmail.com>
2024-10-18 19:29:23 -0400

    Add second modal challenge

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,17 @@
       <p>Paragraph via Slot</p>
     </Modal>
   </div>
+  <div v-if="showModalTwo">
+    <Modal @close="toggleModalTwo">
+      <h1>This is a different Modal</h1>
+      <p>Very different</p>
+      <template v-slot:links>
+        <a href="https://www.google.com/search?client=firefox-b-d&sca_esv=f377f8634278135a&sxsrf=ADLYWIKCbnB0HhgEvhdnyO36ovmnw28_Rw:1729293680659&q=puppies&udm=2&fbs=AEQNm0CFDpRHaDHkXm_YXueHTfHtrgIXUKlluACpCix4T5ZoUSz6e3GWv4zN_09JkP2cR-DwqD-ER1CSuTjXzdXNKT0Wma9mNyum3oWLzhs1xt8u5N_8f3uUgStDs9UpKFAFlFGSOMoqQM9HMiNsP9mioHh2RXyquw6CV17dtB6pod7Wqwuaoj8KLwF-5ybcxBC-9vVePghwqzsfsWfHCa5dRS4yxLs8KA&sa=X&ved=2ahUKEwi_7rD8iJmJAxX2mIkEHcfmBUUQtKgLegQIFBAB&biw=1920&bih=941&dpr=1" target="_blank"> Puppies!</a>
+      </template>
+    </Modal>
+  </div>
   <button @click.shift="toggleModal">Shift Open Modal</button>
+  <button @click="toggleModalTwo">Open Modal 2</button>
   
 </template>
 
@@ -32,7 +42,8 @@ export default {
       header2: "Header Too",
       text: 'string two',
       theme: 'sale',
-      showModal: false
+      showModal: false,
+      showModalTwo: false
     }
   },
   methods: {
@@ -44,9 +55,17 @@ export default {
     },
     toggleModal() {
       this.showModal = !this.showModal
+    },
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo
     }
   }
 }
+
+
+
+
+
 </script>
 
 <style>
